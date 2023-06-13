c4c858f5d6d9fde9a8564cd6b0077d40a072ba1a
bro256 <bronius256@gmail.com>
2023-06-13 15:30:28 +0300

    API POST with basic auth

--- django02/songs/serializers.py
+++ django02/songs/serializers.py
@@ -2,7 +2,20 @@ from rest_framework import serializers
 from . import models
 
 
-class SongSerializer(serializers.ModelSerializer):
+# class SongSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = models.Song
+#         fields = ['name', 'duration', 'band']
+
+# class SongReviewSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = models.SongReview
+#         fields = ['id', 'user', 'song', 'content', 'score']
+
+class SongReviewSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
     class Meta:
-        model = models.Song
-        fields = ['name', 'duration', 'band']
+        model = models.SongReview
+        fields = ['id', 'user', 'user_id', 'song', 'content', 'score']
--- django02/songs/views.py
+++ django02/songs/views.py
@@ -1,7 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from . import models, serializers
 
 
-class SongList(generics.ListAPIView):
-    queryset = models.Song.objects.all()
-    serializer_class = serializers.SongSerializer
+# class SongList(generics.ListAPIView):
+#     queryset = models.Song.objects.all()
+#     serializer_class = serializers.SongSerializer
+
+class SongReviewList(generics.ListAPIView):
+    queryset = models.SongReview.objects.all()
+    serializer_class = serializers.SongReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

a8a6a74d2afa45531bb3e875f98763b6c60d10cc
bro256 <bronius256@gmail.com>
2023-06-13 12:50:49 +0300

    main admins

--- django02/songs/admin.py
+++ django02/songs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Band)
+admin.site.register(models.Song)
+admin.site.register(models.SongReview)
+admin.site.register(models.SongReviewComment)
+admin.site.register(models.SongReviewLike)
